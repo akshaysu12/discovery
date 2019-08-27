011f91eae87b2d780f581584a879d793978c5449
akshay <akshay.sub12@gmail.com>
2019-08-26 22:32:54 -0700

    end to end outline of visualization

--- src/api/webServices/SpotifyService.ts
+++ src/api/webServices/SpotifyService.ts
@@ -3,6 +3,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import btoa from 'btoa';
 import { env } from '../../env';
 import { Service } from 'typedi';
+import { HttpError } from 'routing-controllers';
 
 @Service()
 export class SpotifyService {
@@ -12,29 +13,34 @@ export class SpotifyService {
   public async token(code: string, grantType: string): Promise<object> {
     const auth = `${env.app.spotify.clientId}:${env.app.spotify.clientSecret}`;
     const url = 'https://accounts.spotify.com/api/token';
-    const data = {
+    const body = {
       grant_type: grantType,
     };
 
     if (grantType === 'authorization_code') {
       // tslint:disable: no-string-literal
-      data['code'] = code;
-      data['redirect_uri'] = 'http://localhost:9000/login';
+      body['code'] = code;
+      body['redirect_uri'] = 'http://localhost:9000/login';
     }
-    const options: AxiosRequestConfig = {
-      method: 'POST',
+
+    const options = {
       headers: {
         'content-type': 'application/x-www-form-urlencoded',
         'Authorization': `Basic ${btoa(auth)}`,
       },
-      data: qs.stringify(data),
-      url,
     };
-    const token = axios(options);
-    return token;
+    try {
+      const res = await axios.post(url, qs.stringify(body), options);
+      const data = res.data;
+      return data.access_token;
+
+    } catch (error) {
+      console.log('ERROR: ', error);
+      throw new HttpError(500);
+    }
   }
 
-  public async topTracks(token: object): Promise<object> {
+  public async topTracks(token: object): Promise<object[]> {
     const auth = `Bearer ${token}`;
     const url = 'https://api.spotify.com/v1/me/top/tracks?limit=50';
     const options: AxiosRequestConfig = {
@@ -44,13 +50,15 @@ export class SpotifyService {
       },
       url,
     };
-    const res = axios(options);
-    return res;
+    const res = await axios(options);
+    return res.data.items;
   }
 
   public async features(token: object, tracks: string[]): Promise<object> {
     const auth = `Bearer ${token}`;
-    const url = `https://api.spotify.com/v1/audio-features?ids=${tracks.toString}`;
+    const queryIds = tracks.reduce((accum, id) => accum + '%2C' + id);
+    console.log(queryIds);
+    const url = `https://api.spotify.com/v1/audio-features?ids=${queryIds}`;
     const options: AxiosRequestConfig = {
       method: 'GET',
       headers: {
@@ -58,8 +66,8 @@ export class SpotifyService {
       },
       url,
     };
-    const res = axios(options);
-    return res;
+    const res = await axios(options);
+    return res.data;
   }
 
 }
--- src/web/components/Hello.tsx
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-
-export const Hello = () => <h1>Hello!</h1>;
